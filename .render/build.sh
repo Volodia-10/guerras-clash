de1e594ebd6cc44fca3408f2557f89f4fcf648ff
Jesús Emilio Torres Rodríguez <jesusemiliotorresrodriguez@192.168.1.17>
2025-04-19 22:28:55 -0500

    Instala navegadores Playwright correctamente durante el build

--- .render/build.sh
+++ .render/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Instala las dependencias de Python
+# Instala dependencias de Python
 pip install -r requirements.txt
 
-# Instala los navegadores de Playwright correctamente
-python -m playwright install chromium --with-deps
+# Instala navegadores para Playwright
+python -m playwright install --with-deps
